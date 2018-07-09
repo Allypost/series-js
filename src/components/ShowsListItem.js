82cdb33c9ef3040f4a21c5bf03bc238f6baba7ef
Josip Igrec <me@allypost.net>
2018-07-10 00:26:30 +0200

    Added redneck extra data

--- src/components/ShowsListItem.js
+++ src/components/ShowsListItem.js
@@ -8,10 +8,43 @@ export const ShowProp = PropTypes.shape({
 
 export default class ShowsListItem extends Component {
 
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      showData: {},
+    };
+  }
+
+  async componentDidMount() {
+    const details = await this.fetchDetails();
+
+    // I'm really sorry about this one too
+    // eslint-disable-next-line react/no-set-state,react/no-did-mount-set-state
+    this.setState({ showData: details });
+  }
+
+  async fetchDetails() {
+    const { show } = this.props;
+    const id = show._id;
+
+    const fetcher =
+      (url) => fetch(url)
+        .then((resp) => resp.json())
+        .then((resp) => resp.data);
+
+    try {
+      return await fetcher(`https://api.infinum.academy/api/shows/${id}`);
+    } catch (e) {
+      return {};
+    }
+  }
+
   render() {
     const { show } = this.props;
+    const { showData } = this.state;
     return (
-      <li>
+      <li title={showData.description}>
         {show.title}
       </li>
     );
