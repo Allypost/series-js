96b65f32951a5c2ce2740e16ee40d8c087c054f9
Josip Igrec <me@allypost.net>
2018-07-09 23:41:00 +0200

    Created initial homework app

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      shows: [],
+    };
+
+    this.handleLoadButtonClick = this.handleLoadButtonClick.bind(this);
+  }
+
+  handleLoadButtonClick() {
+    fetch('https://api.infinum.academy/api/shows')
+      .then((resp) => resp.json())
+      .then((resp) => this._updateShows(resp.data))
+      .catch((err) => console.warn(err));
+  }
+
+  _updateShows(data) {
+
+    // FORGIVE ME
+    // eslint-disable-next-line react/no-set-state
+    this.setState({ shows: data });
+  }
+
+  render() {
+    const { shows } = this.state;
+
+    return (
+      <div>
+        <h1>
+          S3RI3S
+        </h1>
+        <ul>
+          {
+            shows.map((show) => (
+              <li key={show._id}>
+                {show.title}
+              </li>))
+          }
+        </ul>
+        <button
+          onClick={this.handleLoadButtonClick}
+          type="button"
+        >
+          Load data
+        </button>
+      </div>
+    );
+  }
+}
