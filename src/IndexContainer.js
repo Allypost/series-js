2268a1e3b7ba44eba452898a20a984840327c0e9
Josip Igrec <me@allypost.net>
2018-07-17 09:07:38 +0200

    Added show display styles

--- src/IndexContainer.js
+++ src/IndexContainer.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { css } from 'emotion';
 
-import LoadingButton from './components/LoadingButton';
+import { ShowCard } from './components/ShowCard';
+
+const showPageContainer = css`
+  display: grid;
+  grid-row: content;
+  grid-column: 1 / span 12;
+  grid-template-columns: repeat(12, 1fr);
+  padding: 2em 0;
+  background-color: #eeeeee;
+`;
+
+const showsContainer = css`
+  display: grid;
+  grid-column: 2 / span 10;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1em 2em;
+`;
 
 export class IndexContainer extends Component {
   constructor(...args) {
@@ -27,14 +44,50 @@ export class IndexContainer extends Component {
     const { isLoading } = this.state;
 
     if (isLoading) {
-      return;
+      return Promise.resolve([]);
     }
 
     this._toggleLoading(true);
 
-    fetch('https://api.infinum.academy/api/shows')
+    return fetch('https://api.infinum.academy/api/shows')
       .then((resp) => resp.json())
-      .then((resp) => this._updateShows(resp.data))
+      .then((resp) => resp.data || [])
+      .then((data) => {
+        this._updateShows(data);
+
+        data.forEach(({ _id }) => this._fetchShowData(_id));
+
+        return data;
+      })
+      .catch((err) => console.warn(err))
+      .finally(() => this._toggleLoading(false));
+  }
+
+  _fetchShowData(showId) {
+    if (!showId) {
+      return {};
+    }
+
+    return fetch(`https://api.infinum.academy/api/shows/${showId}`)
+      .then((resp) => resp.json())
+      .then((resp) => resp.data || {})
+      .then((data) => {
+        const { shows = [] } = this.state;
+
+        shows
+          .slice()
+          .map((show) => {
+            if (show._id === showId) {
+              return Object.assign(show, data);
+            }
+
+            return show;
+          });
+
+        this.setState({ shows });
+
+        return data;
+      })
       .catch((err) => console.warn(err))
       .finally(() => this._toggleLoading(false));
   }
@@ -72,18 +125,15 @@ export class IndexContainer extends Component {
   }
 
   render() {
-    const { isLoading, shows } = this.state;
+    const { shows } = this.state;
     return (
-      <div>
-        <ul className="shows-container collection">
-          {this._renderShows()}
-        </ul>
-        <LoadingButton
-          dataName="Series"
-          hasData={!!shows.length}
-          isLoading={isLoading}
-          onClick={this.handleLoadingButtonClick}
-        />
+      <div className={showPageContainer}>
+        <h2 className={css`grid-column: 2 / span 10;`}>
+          All shows
+        </h2>
+        <div className={showsContainer}>
+          {shows.map((show) => (<ShowCard key={show._id} show={show} />))}
+        </div>
       </div>
     );
   }
