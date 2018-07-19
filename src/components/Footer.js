db1b5bd954e5f93a66594e40255eacd8bca7b4dc
Josip Igrec <me@allypost.net>
2018-07-19 11:21:45 +0200

    Tweaked footer styles to look more uniform

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const container = css`
   grid-column-start: 2;
   grid-column-end: -2;
   grid-row: footer;
-  padding-top: 1.69em;
+  padding: 1.69em 0;
 `;
 
 const logoImage = css`
@@ -17,15 +17,23 @@ const logoImage = css`
 
 const navigation = css`
   justify-self: start;
+  grid-row: 2;
+`;
+
+const navigationLinkContainer = css`
+  padding: 1em 0;
 `;
 
 const listItem = css`
-  display: inline;
-  padding: .5em;
+  padding: .5em .8em;
 `;
 
 const social = css`
+  display: grid;
+  grid-auto-flow: column dense;
+  align-items: center;
   justify-self: end;
+  grid-row: 2;
 `;
 
 const socialImage = css`
@@ -44,45 +52,43 @@ export class Footer extends Component {
         />
 
         <div className={navigation} >
-          <ul>
-            <li className={listItem}>
+          <div className={navigationLinkContainer}>
+            <span className={listItem}>
               About Us
-            </li>
-            <li className={listItem}>
+            </span>
+            <span className={listItem}>
               Privacy Policy
-            </li>
-            <li className={listItem}>
+            </span>
+            <span className={listItem}>
               Terms of Service
-            </li>
-          </ul>
+            </span>
+          </div>
           &#169;
           {(new Date()).getFullYear()}
           &nbsp;Shows. All rights reserved. Additional terms and conditions may apply.
         </div>
         <div className={social}>
-          <ul>
-            <li className={listItem}>
-              <img
-                alt="facebook"
-                className={socialImage}
-                src="/img/ic-facebook@3x.png"
-              />
-            </li>
-            <li className={listItem}>
-              <img
-                alt="twitter"
-                className={socialImage}
-                src="/img/ic-twitter@3x.png"
-              />
-            </li>
-            <li className={listItem}>
-              <img
-                alt="linkedin"
-                className={socialImage}
-                src="/img/ic-linkedin@3x.png"
-              />
-            </li>
-          </ul>
+          <span className={listItem}>
+            <img
+              alt="facebook"
+              className={socialImage}
+              src="/img/ic-facebook@3x.png"
+            />
+          </span>
+          <span className={listItem}>
+            <img
+              alt="twitter"
+              className={socialImage}
+              src="/img/ic-twitter@3x.png"
+            />
+          </span>
+          <span className={listItem}>
+            <img
+              alt="linkedin"
+              className={socialImage}
+              src="/img/ic-linkedin@3x.png"
+            />
+          </span>
         </div>
       </footer>
     );
