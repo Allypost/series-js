c9ce5b70ca74cff2b14f5798eae04e95c207f269
Josip Igrec <me@allypost.net>
2018-07-19 11:43:50 +0200

    Split renderUser method into sub-methods for each case

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -8,23 +8,26 @@ const prettyLink = css`
 `;
 
 export class UserDisplay extends Component {
-  renderUser() {
-    const { user } = this.props;
 
-    if (!user._id) {
-      return (
-        <Link
-          className={prettyLink}
-          to="/login"
-        >
-          Log in
-        </Link>
-      );
-    }
+  // eslint-disable-next-line class-methods-use-this
+  renderLoginLink() {
+    return (
+      <Link
+        className={prettyLink}
+        to="/login"
+      >
+        Log in
+      </Link>
+    );
+  }
+
+  renderUserLink() {
+    const { user } = this.props;
 
     return (
       <Link
         className={prettyLink}
+        onClick={this.handleLogout}
         to="/"
       >
         Hi,
@@ -32,6 +35,16 @@ export class UserDisplay extends Component {
         {user.username}
       </Link>
     );
+  }
+
+  renderUser() {
+    const { user } = this.props;
+
+    if (!user._id) {
+      return this.renderLoginLink();
+    }
+
+    return this.renderUserLink();
   }
 
   render() {
