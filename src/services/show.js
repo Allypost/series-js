aef300672b442ecbec238971986bf690070b0ea6
Josip Igrec <me@allypost.net>
2018-07-21 17:43:10 +0200

    Added mocks for liking/disliking

--- src/services/show.js
+++ src/services/show.js
@@ -1,5 +1,5 @@
 import { observable } from 'mobx';
-import { get as _get } from './api';
+import { get as _get, post as _post } from './api';
 
 export async function getAll(state) {
   state.loadingStates.shows = true;
@@ -13,4 +13,44 @@ export async function get(state, showId) {
   const show = await _get(`shows/${showId}`);
   state.showData = observable(show);
   state.loadingStates.showData = false;
+}
+
+export async function like(state, showId, token) {
+  state.loadingStates.showLike = true;
+
+  /*
+  const resp = await _post(`shows/${showId}/like`, token);
+  Object.assign(state.showData, resp);
+  state.loadingStates.showLike = false;
+  */
+
+  const delay = 100 + (1000 * Math.random());
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      state.loadingStates.showLike = false;
+      const diff = { likesCount: state.showData.likesCount + 1 };
+      const newData = Object.assign(state.showData, diff);
+      resolve(Object.assign({}, newData));
+    }, delay);
+  });
+}
+
+export async function dislike(state, showId, token) {
+  state.loadingStates.showLike = true;
+
+  /*
+  const resp = await _post(`shows/${showId}/dislike`, token);
+  Object.assign(state.showData, resp);
+  state.loadingStates.showLike = false;
+  */
+
+  const delay = 100 + (1000 * Math.random());
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      state.loadingStates.showLike = false;
+      const diff = { likesCount: state.showData.likesCount - 1 };
+      const newData = Object.assign(state.showData, diff);
+      resolve(Object.assign({}, newData));
+    }, delay);
+  });
 }
