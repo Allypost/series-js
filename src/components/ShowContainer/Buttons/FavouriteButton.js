0f57bf08146aa816a50f2a9d180a794471dd2aea
Josip Igrec <me@allypost.net>
2018-07-22 03:46:24 +0200

    Added way to favourite shows

--- src/components/ShowContainer/Buttons/FavouriteButton.js
+++ src/components/ShowContainer/Buttons/FavouriteButton.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { css } from 'emotion';
 
 import { containerStyle as defaultContainerStyle, containerActions as defaultContainerActions, iconStyle as defaultIconStyle, textStyle as defaultTextStyle } from './Bases/SeriesActionButton';
+import state from '../../../state';
+import { observer } from 'mobx-react';
 
 
 const containerStyle = css`
@@ -14,16 +16,54 @@ const iconStyle = css`
   padding: 0.12em .38em 0;
 `;
 
+@observer
 export class FavouriteButton extends Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  isFavourite() {
+    const { favourites, showData } = state;
+
+    return favourites.includes(showData._id);
+  }
+
+  getNewFavouritesList() {
+    const { showData, favourites: oldFavourites } = state;
+
+    if (this.isFavourite()) {
+      return oldFavourites.slice().filter((id) => id !== showData._id);
+    }
+
+    return [showData._id, ...oldFavourites];
+  }
+
+  handleClick(evt) {
+    evt.preventDefault();
+    const newFavourites = this.getNewFavouritesList();
+
+    state.favourites.replace(newFavourites);
+  }
 
   render() {
     return (
-      <div className={containerStyle}>
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+      <div
+        className={containerStyle}
+        onClick={this.handleClick}
+      >
         <span className={iconStyle}>
           ♥
         </span>
         <span className={defaultTextStyle}>
-          Favourite
+          {
+            this.isFavourite()
+              ? 'Un-Favourite'
+              : 'Favourite'
+          }
         </span>
       </div>
     );
--- src/state.js
+++ src/state.js
@@ -1,4 +1,15 @@
-import { observable } from 'mobx';
+import { observable, observe } from 'mobx';
+
+const getFavourites = () => {
+  const store = window.localStorage;
+  const favourites = store.getItem('favourites');
+
+  try {
+    return JSON.parse(favourites) || [];
+  } catch (e) {
+    return [];
+  }
+};
 
 class State {
 
@@ -16,6 +27,9 @@ class State {
   users = [];
 
   @observable
+  favourites = [];
+
+  @observable
   loadingStates = {
     shows: false,
     showData: false,
@@ -23,6 +37,18 @@ class State {
     episodes: false,
     episodeData: observable.object(),
   };
+
+  constructor() {
+    this.favourites.replace(getFavourites());
+
+    observe(this.favourites, (...args) => {
+      const favourites = this.favourites.slice();
+      const store = window.localStorage;
+
+      store.setItem('favourites', JSON.stringify(favourites));
+    });
+  }
+
 }
 
 export default new State();
