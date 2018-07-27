820852966f17f83a85187089ddd250fad998e301
Josip Igrec <me@allypost.net>
2018-07-27 04:39:10 +0200

    Now (almost) adhering to desired design and functionality (file stuff missing)

--- src/components/ShowContainer/Show/AddEpisode.js
+++ src/components/ShowContainer/Show/AddEpisode.js
@@ -1,77 +1,106 @@
 import React, { Component } from 'react';
-import { css } from 'emotion';
+import { Link } from 'react-router-dom';
+import { css, cx } from 'emotion';
 import { action, observable } from 'mobx';
 import { observer } from 'mobx-react';
 
-const modalHeader = css`
-  font-size: 2.4em;
-  text-align: center;
-  margin-top: 0;
-  color: #ff758c;
-`;
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
+import Textarea from 'muicss/lib/react/textarea';
+import Option from 'muicss/lib/react/option';
+import Select from 'muicss/lib/react/select';
+import Button from 'muicss/lib/react/button';
 
 const container = css`
   postition: relative;
+  display: grid;
+  min-width: 400px;
+  grid-row-gap: 1em;
 `;
 
-const inputHeader = css`
-  margin-bottom: 0;
-
-  &:first-child {
-    margin-top: 0;
-  }
-`;
-
-const input = css`
-  border: none;
-  outline: none;
-  color: #ff758c;
-  background-color: transparent;
-  border-bottom: 2px solid #ff758c;
-  font-kerning: normal;
-  font-size: 1em;
-`;
-
-const titleStyle = css`
-  ${input}
-  font-size: 1.5em;
-`;
-
-const descriptionStyle = css`
-  ${input}
-  width: 31.7vw;
-  height: 4.5em;
-  resize: none;
-`;
-
-const numbersStyle = css`
-  ${input}
-  width: 2.5em;
-`;
-
-const actionsContainer = css`
-  margin-top: 2em;
-`;
-
-const button = css`
-  border: 1px solid #e0e0e0;
-  background: #ffffff;
-  font-size: 1.2em;
-  padding: .8em 1.4em .7em;
-  border-radius: 6px;
-  cursor: pointer;
-  font-weight: bold;
-  text-shadow: 1px 1px 0 rgba(0, 0, 0, .15);
+const modalHeader = css`
+  font-size: 1.63em !important;
+  font-weight: 300;
+  color: #212121;
 `;
 
 const submitButton = css`
-  ${button}
-  color: #2e7d32;
+  justify-self: center;
+  border-radius: 6px;
+  background-color: #ff758c;
+  font-size: 1em;
+  padding: .3em 1.8em;
+  box-sizing: content-box;
+
+  &:hover {
+    background-color: #ff6680;
+  }
+`;
+
+const selectInput = css`
+  padding: 0 .5em;
+  width: 100%;
+  box-sizing: border-box;
+  margin-bottom: 0;
+
+  & > select {
+    color: #ff758c;
+  }
+
+  &:focus > select {
+    border-color: #ff758c;
+  }
+`;
+
+const selectLabel = css`
+  line-height: 1.1em;
+  vertical-align: middle;
+`;
+
+const inputStyle = css`
+  & > input:focus {
+    border-color: #ff758c;
+
+    & ~ label {
+      color: #ff758c;
+    }
+  }
+`;
+
+const seasonEpisodeContainer = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const selectContainer = css`
+  display: grid;
+  align-items: baseline;
+  justify-items: center;
+  grid-auto-flow: column;
+  grid-template-columns: fit-content(0) auto;
+`;
+
+const descriptionStyle = css`
+  & > textarea:focus {
+    border-color: #ff758c;
+
+    & ~ label {
+      color: #ff758c;
+    }
+  }
 `;
 
 const cancelButton = css`
-  ${button}
-  color: #ff6f00;
+  position: absolute;
+  top: .3em;
+  right: .63em;
+  width: 3.141592em;
+  height: 3.141592em;
+  line-height: 3.3em;
+`;
+
+const cancelButtonIcon = css`
+  line-height: inherit;
 `;
 
 export class AddEpisode extends Component {
@@ -82,6 +111,8 @@ export class AddEpisode extends Component {
     description: '',
     season: 1,
     episode: 1,
+    seasons: [...Array(8).keys()].map((i) => i + 1),
+    episodes: [...Array(40).keys()].map((i) => i + 1),
   }
 
   get containerClass() {
@@ -118,82 +149,102 @@ export class AddEpisode extends Component {
       title, description, episode, season,
     } = this.componentState;
 
+    const {
+      episodes, seasons,
+    } = this.componentState;
+
     const { onClose } = this.props;
 
     return (
-      <div className={this.containerClass}>
-        <form
-          action="#"
-          method="POST"
+      <div>
+        <Form
+          className={container}
+          onSubmit={this.handleAddEpisode}
         >
-          <h1 className={modalHeader}>
-            Add episode
-          </h1>
-          <h1 className={inputHeader}>
-            Title
-          </h1>
-          <div>
-            <input
-              className={titleStyle}
-              onChange={this.handleInputChange('title')}
-              required
-              type="text"
-              value={title}
-            />
+          <legend className={modalHeader}>
+            Add new episode
+          </legend>
+          <Input
+            className={inputStyle}
+            floatingLabel
+            label="Episode title"
+            onChange={this.handleInputChange('title')}
+            required
+            value={title}
+          />
+          <div className={seasonEpisodeContainer}>
+            <div className={selectContainer}>
+              <span className={selectLabel}>
+                Season:
+              </span>
+              <Select
+                className={selectInput}
+                defaultValue={season}
+                onChange={this.handleInputChange('season')}
+                required
+              >
+                {
+                  seasons
+                    .map((i) => (
+                      <Option
+                        key={i}
+                        label={i}
+                        value={i}
+                      />
+                    ))
+                }
+              </Select>
+            </div>
+            <div className={selectContainer}>
+              <span className={selectLabel}>
+                Episode:
+              </span>
+              <Select
+                className={selectInput}
+                defaultValue={episode}
+                onChange={this.handleInputChange('episode')}
+                required
+              >
+                {
+                  episodes
+                    .map((i) => (
+                      <Option
+                        key={i}
+                        label={i}
+                        value={i}
+                      />
+                    ))
+                }
+              </Select>
+            </div>
           </div>
-          <h1 className={inputHeader}>
-            Description
-          </h1>
-          <div>
-            <textarea
-              className={descriptionStyle}
-              onChange={this.handleInputChange('description')}
-              required
-              value={description}
-            />
-          </div>
-          <h1 className={inputHeader}>
-            Season and Episode
-          </h1>
-          <div>
-            <span>
-              S
-            </span>
-            <input
-              className={numbersStyle}
-              onChange={this.handleInputChange('season')}
-              required
-              type="number"
-              value={season}
-            />
-            <span>
-              Ep
-            </span>
-            <input
-              className={numbersStyle}
-              onChange={this.handleInputChange('episode')}
-              required
-              type="number"
-              value={episode}
-            />
-          </div>
-          <div className={actionsContainer}>
-            <button
-              className={submitButton}
-              onClick={this.handleAddEpisode}
-              type="button"
-            >
-              Save
-            </button>
-            <button
-              className={cancelButton}
-              onClick={onClose}
-              type="button"
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
+          <Textarea
+            className={descriptionStyle}
+            floatingLabel
+            label="Episode description"
+            onChange={this.handleInputChange('description')}
+            value={description}
+          />
+          <Button
+            className={submitButton}
+            color="accent"
+          >
+            Add new episode
+          </Button>
+        </Form>
+        <Link
+          onClick={onClose}
+          to="/"
+        >
+          <Button
+            className={cancelButton}
+            variant="fab"
+          >
+            <i className={cx('material-icons', cancelButtonIcon)}>
+              close
+            </i>
+          </Button>
+        </Link>
       </div>
     );
   }
--- src/containers/ShowContainerModal.js
+++ src/containers/ShowContainerModal.js
@@ -4,10 +4,19 @@ import { action } from 'mobx';
 
 import { Modal } from '../components/_global/Modal';
 import { AddEpisode } from '../components/ShowContainer/Show/AddEpisode';
+import { add as addEpisode } from '../services/episode';
 
 @inject('state')
 @observer
 export class ShowContainerModal extends Component {
+
+  get showId() {
+    const { match } = this.props;
+    const { params } = match;
+    const { showId } = params;
+
+    return showId;
+  }
 
   @action.bound
   handleClose(evt) {
@@ -18,12 +27,38 @@ export class ShowContainerModal extends Component {
     history.push('./');
   }
 
+  @action.bound
+  handleAddEpisode(evt, data) {
+    evt.preventDefault();
+
+    const { state } = this.props;
+    const { user } = state;
+    const { token } = user;
+
+    const postData = {
+      showId: this.showId,
+      title: data.title,
+      description: data.description,
+      episodeNumber: String(data.episode),
+      season: String(data.season),
+    };
+
+    addEpisode(state, token, postData);
+  }
+
   render() {
+    const { state } = this.props;
+    const { loadingStates } = state;
+
+    const { addEpisode: isLoading } = loadingStates;
+
     return (
       <Modal
         onClose={this.handleClose}
       >
         <AddEpisode
+          isLoading={isLoading}
+          onAdd={this.handleAddEpisode}
           onClose={this.handleClose}
           show
         />
